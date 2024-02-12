3964949cc777ef56997aa76f5b4617418b35839b
GaocanWang <ericwang200812@gmail.com>
2024-02-12 15:51:52 -0500

    coded indexsystem

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,19 +1,21 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
-/**
- * The Constants class provides a convenient place for teams to hold robot-wide numerical or boolean
- * constants. This class should not be used for any other purpose. All constants should be declared
- * globally (i.e. public static). Do not put anything functional in this class.
- *
- * <p>It is advised to statically import this class (or one of its inner classes) wherever the
- * constants are needed, to reduce verbosity.
- */
 public final class Constants {
-  public static class OperatorConstants {
-    public static final int kDriverControllerPort = 0;
-  }
+    public static class Mapping {
+        public static class Index {
+            public static int indexMotor = 0;
+        }
+    }
+
+    public static class Generic {
+        public static int timeoutMs = 3000;
+    }
+
+    public static class Config {
+        public static class Drive {
+            public static class Power {
+                public static double kOpenLoopRamp = 0.0;
+            }
+        }
+    }
 }
---
+++ src/main/java/frc/robot/subsystems/IndexSystem.java
@@ -0,0 +1,45 @@
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+
+import com.ctre.phoenix.motorcontrol.NeutralMode;
+import com.ctre.phoenix.motorcontrol.TalonSRXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+
+import ca.team4308.absolutelib.wrapper.MotoredSubsystem;
+import edu.wpi.first.util.sendable.Sendable;
+import frc.robot.Constants;
+
+public class IndexSystem extends MotoredSubsystem {
+    public final TalonSRX motor;
+    public boolean state;
+
+    public IndexSystem() {
+        // Setup Controllers
+        motor = new TalonSRX(Constants.Mapping.Index.indexMotor);
+
+        motor.configFactoryDefault(Constants.Generic.timeoutMs);
+        motor.configOpenloopRamp(Constants.Config.Drive.Power.kOpenLoopRamp, Constants.Generic.timeoutMs);
+        motor.configClosedloopRamp(0.1, Constants.Generic.timeoutMs);
+        motor.setNeutralMode(NeutralMode.Brake);
+        motor.configNeutralDeadband(0.001, Constants.Generic.timeoutMs);
+        motor.changeMotionControlFramePeriod(5);
+        motor.configVoltageCompSaturation(12.5, Constants.Generic.timeoutMs);
+        motor.enableVoltageCompensation(true);
+
+        stopControllers();
+    }
+
+    public void setIndexOutput(double output) {
+        motor.set(TalonSRXControlMode.PercentOutput, output);
+    }
+
+    public void stopControllers() {
+        motor.set(TalonSRXControlMode.PercentOutput, 0.0);
+    }
+
+    @Override
+    public Sendable log() {
+        return this;
+    }
+}
