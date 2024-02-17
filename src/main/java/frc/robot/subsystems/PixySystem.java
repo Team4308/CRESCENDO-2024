492a49b1aae66dc0acc02aa02f928a0972db2713
Max <nguyenmax2008@gmail.com>
2024-02-17 15:24:02 -0500

    basic object detection using pixy, needs to be tested

---
+++ src/main/java/frc/robot/subsystems/PixySystem.java
@@ -0,0 +1,53 @@
+package frc.robot.subsystems;
+
+import java.util.ArrayList;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.pixy2api.Pixy2;
+import frc.robot.pixy2api.Pixy2CCC;
+import frc.robot.pixy2api.Pixy2CCC.Block;
+import frc.robot.pixy2api.links.SPILink;
+
+public class PixySystem extends SubsystemBase {
+  private Pixy2 pixy;
+  private int targetCount;
+  private int sig = Pixy2CCC.CCC_SIG1;
+  private ArrayList<Block> targets;
+
+  public PixySystem() {
+    pixy = Pixy2.createInstance(new SPILink());
+    pixy.init();
+    targets = new ArrayList<Block>();
+  }
+
+  @Override
+  public void periodic() {
+    targetCount = pixy.getCCC().getBlocks(false, sig, 11);
+    SmartDashboard.putNumber("Targets found", targetCount);
+    targets = pixy.getCCC().getBlockCache();
+    findClosestTarget();
+    // This method will be called once per scheduler run
+  }
+
+  public int[] findClosestTarget(){
+    int[] directionSize = new int[2]; // index 0 is amount from center of view from -157 to 157, 1 is width of the target
+    if(targetCount == 0){
+      directionSize[0] = 200; //set outside range to know the target is not in view
+      return directionSize;
+    }
+    // searching for the largest target in view which should be the closest
+    Block closestTarget = targets.get(0);
+    for(int index = 1; index<targets.size(); index++){
+      if(targets.get(index).getWidth()>closestTarget.getWidth()){
+        closestTarget = targets.get(index);
+      }
+    }
+    directionSize[0]=closestTarget.getX()-157;//setting the center of the camera view to be center.  
+    directionSize[1]=closestTarget.getWidth();
+    SmartDashboard.putNumber("Largest x location", directionSize[0]);
+    SmartDashboard.putNumber("Largest width", directionSize[1]);
+    return directionSize;
+  }
+
+}
