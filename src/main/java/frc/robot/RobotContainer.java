79e6ad9717b3dece7c8fffffbba78cebfb89ca19
GaocanWang <ericwang200812@gmail.com>
2024-02-26 15:56:12 -0500

    removed unnecessary robotcontainer stuff

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -6,15 +6,11 @@ package frc.robot;
 
 import java.util.ArrayList;
 
-import ca.team4308.absolutelib.control.JoystickHelper;
 import ca.team4308.absolutelib.control.XBoxWrapper;
-import ca.team4308.absolutelib.math.DoubleUtils;
-import ca.team4308.absolutelib.math.Vector2;
 import ca.team4308.absolutelib.wrapper.LogSubsystem;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
 import frc.robot.commands.IntakeCommand;
 import frc.robot.subsystems.IntakeSystem;
 
@@ -29,7 +25,6 @@ public class RobotContainer {
   
 
   //Controllers
-  public final XBoxWrapper stick1 = new XBoxWrapper(0);
   public final XBoxWrapper stick2 = new XBoxWrapper(1);
 
   //Auto
