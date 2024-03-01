30c1d9b7f41800e11b8e3525469d897bd62d937f
Max <nguyenmax2008@gmail.com>
2024-03-01 15:58:50 -0500

    broken code

--- src/main/java/frc/robot/subsystems/PixySystem.java
+++ src/main/java/frc/robot/subsystems/PixySystem.java
@@ -15,45 +15,60 @@ import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
 
 public class PixySystem extends SubsystemBase {
   private Pixy2 pixy;
-  private int targetCount;
   private int sig = Pixy2CCC.CCC_SIG1;
-  private ArrayList<Block> targets;
 
   public PixySystem() {
     pixy = Pixy2.createInstance(new SPILink());
     pixy.init();
     pixy.setLamp((byte) 1, (byte) 1); // Turns the LEDs on
 		pixy.setLED(255, 255, 255); // Sets the RGB LED to full white
-    targets = new ArrayList<Block>();
+    pixy.getVideo();
   }
 
   @Override
   public void periodic() {
-    targetCount = pixy.getCCC().getBlocks(false, sig, 8);
-    SmartDashboard.putNumber("Targets found", targetCount);
-    targets = pixy.getCCC().getBlockCache();
-    findClosestTarget();
+    getClosestTarget();
+    SmartDashboard.putNumber("Target Width", getTargetWidth(getClosestTarget()));
+    SmartDashboard.putNumber("Target X", getTargetX(getClosestTarget()));
     // This method will be called once per scheduler run
   }
 
-  public int[] findClosestTarget(){
-    int[] directionSize = new int[2]; // index 0 is amount from center of view from -157 to 157, 1 is width of the target
-    if(targetCount == 0){
-      directionSize[0] = 200; //set outside range to know the target is not in view
-      return directionSize;
+  public Block getClosestTarget() {
+		// Gets the number of "blocks", identified targets, that match signature 1 on the Pixy2,
+		// does not wait for new data if none is available,
+		// and limits the number of returned blocks to 25, for a slight increase in efficiency
+		int targetCount = pixy.getCCC().getBlocks(false, sig, 10);
+    SmartDashboard.putNumber("Targets", targetCount);
+		if (targetCount <= 0) {
+			return null; // If blocks were not found, stop processing
+		}
+		ArrayList<Block> targets = pixy.getCCC().getBlockCache(); // Gets a list of all blocks found by the Pixy2
+		Block closestTarget = null;
+		for (Block target : targets) { // Loops through all blocks and finds the widest one
+			if (closestTarget == null) {
+				closestTarget = target;
+			} else if (target.getWidth() > closestTarget.getWidth()) {
+				closestTarget = target;
+			}
+		}
+
+		return closestTarget;
+	}
+
+  public int getTargetWidth(Block target) {
+    if (target == null) {
+      return 0; 
     }
-    // searching for the largest target in view which should be the closest
-    Block closestTarget = targets.get(0);
-    for(int index = 1; index<targets.size(); index++){
-      if(targets.get(index).getWidth()>closestTarget.getWidth()){
-        closestTarget = targets.get(index);
-      }
+    int targetWidth = target.getWidth();
+    return targetWidth;
+  }
+
+  public int getTargetX(Block target) {
+    if (target == null) {
+      return 1000; //set outside range to know the target is not in view
     }
-    directionSize[0]=closestTarget.getX()-157;//setting the center of the camera view to be center.  
-    directionSize[1]=closestTarget.getWidth();
-    SmartDashboard.putNumber("Largest x location", directionSize[0]);
-    SmartDashboard.putNumber("Largest width", directionSize[1]);
-    return directionSize; 
+    int targetX = target.getX();
+    return targetX;
   }
 
 }
