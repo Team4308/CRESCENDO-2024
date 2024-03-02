c366e2002d285e4140c436d4fe07ace901c3c8a9
bluefeng2 <57335144+bluefeng2@users.noreply.github.com>
2024-03-01 20:36:05 -0500

    added pid, cleaned up code

--- src/main/java/frc/robot/commands/RotateShooterCommand.java
+++ src/main/java/frc/robot/commands/RotateShooterCommand.java
@@ -2,17 +2,24 @@ package frc.robot.commands;
 
 import java.util.function.Supplier;
 
+import frc.robot.Constants;
 import frc.robot.subsystems.RotateShooterSystem;
+import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.wpilibj2.command.Command;
+
 
 public class RotateShooterCommand extends Command {
 
     private final RotateShooterSystem m_subsystem;
-    private final Supplier<Double> control;
+    private final Supplier<Double> degree;
 
-    public RotateShooterCommand(RotateShooterSystem subsystem, Supplier<Double> control) {
+    public final PIDController pidController;
+
+    public RotateShooterCommand(RotateShooterSystem subsystem, Supplier<Double> degree) {//degree should be in between 16-43 degrees FOR NOW
         m_subsystem = subsystem;
-        this.control = control;
+        this.degree = degree;
+
+        pidController = new PIDController(Constants.PID.Shooter.kP, Constants.PID.Shooter.kI, Constants.PID.Shooter.kD);
 
         addRequirements(subsystem);
     }
@@ -23,8 +30,20 @@ public class RotateShooterCommand extends Command {
 
     @Override
     public void execute() {
-        double control = this.control.get();
-        m_subsystem.setMotorOutput(control);
+        double wantedDegree = this.degree.get();
+        //0 is base postiion(16 degrees)
+        // 14.25925757 is max position(43 degree)
+        //2200/12      ratio  
+        //28 degrees 16-43
+
+        double m = 16/(43-14.259);
+        double b = (16*m);
+        double outputDegree = m*wantedDegree-b;
+
+        double encoderDegree = m_subsystem.getMotorPosition();
+
+        double motorDegree = pidController.calculate(encoderDegree, outputDegree);
+        m_subsystem.setMotorOutput(motorDegree);
     }
 
     @Override
--- src/main/java/frc/robot/subsystems/RotateShooterSystem.java
+++ src/main/java/frc/robot/subsystems/RotateShooterSystem.java
@@ -9,14 +9,26 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import com.ctre.phoenix6.hardware.TalonFX;
 import frc.robot.Constants;
 
+
 public class RotateShooterSystem extends SubsystemBase {
     public final TalonFX motor;
+
     public RotateShooterSystem() {
         motor = new TalonFX(Constants.Mapping.Shooter.motor);
     }
 
     public void setMotorOutput(double percent){
         motor.set(percent);
+    }
+
+    public double getMotorPosition() {
+        var rotorPosSignal = motor.getRotorPosition();
+        var rotorPos = rotorPosSignal.getValue();
+
+        return rotorPos;
+    }
+    public void resetSensors() {
+        motor.setPosition(0);
     }
 
     public void stopControllers(){
