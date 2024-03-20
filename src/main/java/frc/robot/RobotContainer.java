c10740663bfcf856f2f3f5fb51a12b86ad13cf5a
Max <nguyenmax2008@gmail.com>
2024-03-20 14:51:17 -0400

    middle auton and changed shooter speed to 80

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -116,14 +116,15 @@ public class RobotContainer
 
     NamedCommands.registerCommand("IntakeCommand", new IntakeCommand(m_intakeSystem, () -> -1.0));
     NamedCommands.registerCommand("IndexCommand", new IndexCommand(m_indexSystem, () -> -1.0));
-    NamedCommands.registerCommand("ShooterCommand", new ShooterCommand(m_shooterSubsystem, () -> 90.0));
+    NamedCommands.registerCommand("ShooterCommand", new ShooterCommand(m_shooterSubsystem, () -> 80.0));
     //NamedCommands.registerCommand("SpeakerAlignTrue", new InstantCommand(() -> drivebase.alignToSpeaker(true)));
     //NamedCommands.registerCommand("SpeakerAlignFalse", new InstantCommand(() -> drivebase.alignToSpeaker(false)));
     NamedCommands.registerCommand("SpeakerAlign", new InstantCommand(() -> drivebase.speakerAlignCommand()));
     NamedCommands.registerCommand("ResetGyro", new InstantCommand(drivebase::zeroGyro));
     NamedCommands.registerCommand("AutoAlignShooter", new RotateShooterCommand(m_rotateShooterSystem, () -> m_rotateShooterSystem.autoAlignShooter()));
     NamedCommands.registerCommand("BeambreakCommand", new BeambreakCommand(() -> getBeambreakControl()));
-    NamedCommands.registerCommand("SubwooferShooterRotate", new RotateShooterCommand(m_rotateShooterSystem, () -> Constants.GamePieces.speaker.angle));
+    NamedCommands.registerCommand("SubwooferAngle", new RotateShooterCommand(m_rotateShooterSystem, () -> Constants.GamePieces.speaker.angle));
+    NamedCommands.registerCommand("MiddleNoteAngle", new RotateShooterCommand(m_rotateShooterSystem, () -> 36.0));
     
     //Command Instantiations
     intakeCommand = new IntakeCommand(m_intakeSystem, () -> getIntakeControl());
@@ -317,7 +318,7 @@ public class RobotContainer
   }
   
   public double getShooterControl() {
-    return stick1.getRightTrigger() * 90;  //converting into RPS
+    return stick1.getRightTrigger() * 80;  //converting into RPS
   }
 
   public boolean getBeambreakControl() {
