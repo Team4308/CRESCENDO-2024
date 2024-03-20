a32e520d34e6c8e7d64d8e521b954b27c403593d
GaocanWang <ericwang200812@gmail.com>
2024-03-20 11:35:40 -0400

    increased max speed

--- src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
+++ src/main/java/frc/robot/subsystems/swervedrive/SwerveSubsystem.java
@@ -61,7 +61,7 @@ public class SwerveSubsystem extends SubsystemBase
   /**
    * Maximum speed of the robot in meters per second, used to limit acceleration.
    */
-  public        double      maximumSpeed = Units.feetToMeters(14.5);
+  public        double      maximumSpeed = Units.feetToMeters(15.1);
 
   /**
    * Initialize {@link SwerveDrive} with the directory provided.
